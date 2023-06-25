40c75af0720d08e1db930f9c87e99c2b0e3d83fe
Moghaddm <mahdimoghaddam2005@gmail.com>
2023-06-25 14:17:10 +0330

    Server Entity

--- BigBlueApi/Domain/Server.cs
+++ BigBlueApi/Domain/Server.cs
@@ -1,6 +1,36 @@
 namespace BigBlueApi.Domain;
+
 public class Server
 {
-    public string ServerUrl { get; set; }
-    public string SharedSecret { get; set; }
+    public int Id { get; }
+    public string ServerUrl { get; private set; }
+    public string SharedSecret { get; private set; }
+    public int ServerLimit { get; private set; }
+    public List<Session> Sessions { get; set; }
+
+    public Server(string serverUrl, string sharedSecret, int serverLimit)
+    {
+        if (string.IsNullOrWhiteSpace(serverUrl))
+            throw new ArgumentNullException($"{nameof(serverUrl)} is Null Or Invalid.");
+        if (string.IsNullOrEmpty(sharedSecret))
+            throw new ArgumentNullException($"{nameof(sharedSecret)} is Null Or Invalid");
+        if (serverLimit <= 0)
+            throw new ArgumentOutOfRangeException("Server Limit Cant Be Less Than ZERO.");
+        ServerUrl = serverUrl;
+        SharedSecret = sharedSecret;
+        ServerLimit = serverLimit;
+    }
+
+    public void UpdateServer(string serverUrl, string sharedSecret, int serverLimit)
+    {
+        if (string.IsNullOrWhiteSpace(serverUrl))
+            throw new ArgumentNullException($"{nameof(serverUrl)} is Null Or Invalid.");
+        if (string.IsNullOrEmpty(sharedSecret))
+            throw new ArgumentNullException($"{nameof(sharedSecret)} is Null Or Invalid");
+        if (serverLimit <= 0)
+            throw new ArgumentOutOfRangeException("Server Limit Cant Be Less Than ZERO.");
+        ServerUrl = serverUrl;
+        SharedSecret = sharedSecret;
+        ServerLimit = serverLimit;
+    }
 }
