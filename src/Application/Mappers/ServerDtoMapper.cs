8547d1173198ecec73daae1ec83ad747b73053b3
Moghaddm <mahdimoghaddam2005@gmail.com>
2023-06-26 13:45:16 +0330

    Update DTO Server

--- src/Application/Mappers/ServerDtoMapper.cs
+++ src/Application/Mappers/ServerDtoMapper.cs
@@ -6,8 +6,8 @@ namespace BigBlueApi.Application.Mappers;
 public static class ServerDtoMapper
 {
     public static ServerAddEditDto Map(Server server) =>
-        new ServerAddEditDto(server.ServerUrl, server.SharedSecret, server.ServerLimit);
+        new ServerAddEditDto(server.ServerUrl, server.ServerLimit);
 
-    public static Server Map(ServerAddEditDto server) =>
-        new Server(server.ServerUrl, server.SharedSecret, server.ServerLimit);
+    public static Server Map(ServerAddEditDto server)
+        => new Server(server.ServerUrl,server.ServerLimit);
 }
