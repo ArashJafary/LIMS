1eb044863d7279c3f9b3cc55bc17ee84105b1718
Mohammad Mahdi <MahdiMoghaddam2005@gmail.com>
2023-06-26 14:36:55 +0330

    Revert "Repository Service Added"

--- BigBlueApi/Domain/IRepository/ISessionRepository.cs
+++
@@ -1,8 +0,0 @@
-namespace BigBlueApi.Domain.IRepository;
-public interface ISessionRepository 
-{
-    ValueTask<string> CreateSession(Session session);
-    ValueTask<Session> Find(string meetingID);
-    Task EditSession(string id,Session session);
-    Task StopRunnig(string id);
-}
