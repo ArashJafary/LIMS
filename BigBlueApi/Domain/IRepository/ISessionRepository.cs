7248425d8ba08e08878afda0dcab8e2650d8da44
ArashJafary <aj2774382@gmail.com>
2023-06-26 11:19:28 +0330

    add find to interfac session

--- BigBlueApi/Domain/IRepository/ISessionRepository.cs
+++ BigBlueApi/Domain/IRepository/ISessionRepository.cs
@@ -2,6 +2,7 @@ namespace BigBlueApi.Domain.IRepository;
 public interface ISessionRepository 
 {
     ValueTask<string> CreateSession(Session session);
+    ValueTask<Session> Find(string meetingID);
     Task EditSession(string id,Session session);
     Task StopRunnig(string id);
 }
