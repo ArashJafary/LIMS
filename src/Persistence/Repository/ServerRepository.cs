462878f153320abb3b7df1b5c87a2e90f41ecb27
Moghaddm <mahdimoghaddam2005@gmail.com>
2023-06-26 13:46:37 +0330

    Update Server Repository

--- src/Persistence/Repository/ServerRepository.cs
+++ src/Persistence/Repository/ServerRepository.cs
@@ -28,7 +28,7 @@ public class ServerRepository : IServerRepository
     public async ValueTask EditServer(int id, Server server)
     {
         var newServer = await _servers.FirstOrDefaultAsync(server => server.Id == id);
-        newServer!.UpdateServer(server.ServerUrl, server.SharedSecret, server.ServerLimit);
+        newServer!.UpdateServer(server.ServerUrl,server.SharedSecret, server.ServerLimit);
         _servers.Update(newServer);
     }
 
