895d3ac6d98a5caa04b458024f6c8017d7525c07
Moghaddm <mahdimoghaddam2005@gmail.com>
2023-07-02 14:44:15 +0330

    Update All Architecture

---
+++ src/LMS.Application/Services/ServerServiceImp.cs
@@ -0,0 +1,42 @@
+using BigBlueApi.Application.DTOs;
+using BigBlueApi.Domain;
+using BigBlueApi.Domain.IRepository;
+
+namespace BigBlueApi.Persistence.Repository;
+
+public class ServerServiceImp 
+{
+    private readonly IServerRepository _servers;
+    private readonly IUnitOfWork _unitOfWork;
+
+    public ServerServiceImp(IServerRepository serverRepository,IUnitOfWork unitOfWork)
+    {
+        _servers = serverRepository;
+        _unitOfWork = unitOfWork;
+    }
+
+    public async ValueTask<bool> CanJoinServer(int id)
+    {
+      return await _servers.CanJoinServer(id);
+    }
+
+    public async ValueTask<int> CreateServer(ServerAddEditDto serverAddEditDto)
+    {
+        var server =await _servers.CreateServer(ServerDtoMapper.Map(serverAddEditDto));
+        await _unitOfWork.SaveChangesAsync();
+        return server.Id;
+    }
+
+    public async ValueTask EditServer(int id, ServerAddEditDto serverAddEditDto)
+    {
+        await _servers.EditServer(id, ServerDtoMapper.Map(serverAddEditDto));
+        await _unitOfWork.SaveChangesAsync();
+    }
+
+    public async ValueTask<ServerAddEditDto> MostCapableServer()
+    {
+        var server = await _servers.MostCapableServer();
+        var ServerDto=ServerDtoMapper.Map(server);
+        return ServerDto;
+    }
+}
---
+++ src/LMS.Infrastructure/BigBlueContextFactory.cs
@@ -0,0 +1,17 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace BigBlueApi.Persistence;
+
+public class BigBlueContextFactory : IDesignTimeDbContextFactory<BigBlueContext>
+{
+    public BigBlueContext CreateDbContext(string[] args)
+    {
+        var _configuration = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+        var optionsBuilder = new DbContextOptionsBuilder<BigBlueContext>();
+
+        optionsBuilder.UseSqlServer(_configuration.GetConnectionString("MSSQL"));
+        return new BigBlueContext(optionsBuilder.Options);
+    }
+}
