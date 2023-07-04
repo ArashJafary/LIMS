d8ef0bdd12a365514e1282af534f939eefdc2ba3
Moghaddm <mahdimoghaddam2005@gmail.com>
2023-07-04 14:58:50 +0330

    Update User Class and Interface Repository

--- src/LMS.Domain/IRepositories/IUserRepository.cs
+++ src/LMS.Domain/IRepositories/IUserRepository.cs
@@ -3,7 +3,9 @@ using LIMS.Domain.Entity;
 namespace BigBlueApi.Domain.IRepository;
 public interface IUserRepository
 {
-    ValueTask<int> CreateUser(User user);
-    ValueTask<User> Find(int userId);
-    Task EditUser(int id,User user);
+    ValueTask<long> CreateUser(User user);
+    ValueTask<User> GetUser(long userId);
+    ValueTask<IEnumerable<User>> GetAllUsers();
+    Task<long> DeleteUser(long userId);
+    Task EditUser(long id, User user);
 }
