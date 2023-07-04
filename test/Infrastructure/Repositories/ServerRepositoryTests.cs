28a5604516a37cf5958d117caed89eae742da03b
Moghaddm <mahdimoghaddam2005@gmail.com>
2023-07-04 08:52:35 +0330

    Update Namespace

--- test/Infrastructure/Repositories/ServerRepositoryTests.cs
+++ test/Infrastructure/Repositories/ServerRepositoryTests.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using BigBlueApi.Persistence.Repository;
+using BigBlueApi.Persistence.Repositories;
 using LIMS.Domain.Entity;
 using LIMS.Infrastructure.Persistence;
 using Microsoft.EntityFrameworkCore;
