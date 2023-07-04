ed17cd048f14f035f2ff5f63cfaf26036a1204ac
Moghaddm <mahdimoghaddam2005@gmail.com>
2023-07-04 14:58:06 +0330

    Update Server Repositories

--- src/LMS.Domain/IRepositories/IServerRepository.cs
+++ src/LMS.Domain/IRepositories/IServerRepository.cs
@@ -4,8 +4,11 @@ namespace BigBlueApi.Domain.IRepository;
 
 public interface IServerRepository
 {
-    ValueTask<bool> CanJoinServer(int id);
-    ValueTask<Server> CreateServer(Server server);
-    ValueTask EditServer(int id, Server server);
+    ValueTask<bool> CanJoinServer(long id);
+    Task<long> DeleteServer(long Id);
+    ValueTask<Server> GetServer(long Id);
+    ValueTask<List<Server>> GetAllServers();
+    Task<Server> CreateServer(Server server);
     ValueTask<Server> MostCapableServer();
+    Task UpdateServer(int id, Server server);
 }
