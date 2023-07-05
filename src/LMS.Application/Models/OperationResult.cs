a457d92f15738e63800e899865e05b46d0032090
ArashJafary <aj2774382@gmail.com>
2023-07-05 10:41:24 +0330

    separate opration result 1 whit data 2 whitout data

--- src/LMS.Application/Models/OperationResult.cs
+++ src/LMS.Application/Models/OperationResult.cs
@@ -1,16 +1,45 @@
 namespace BigBlueApi.Models;
-public class OperationResult<TResult>
+
+public class OperationResult
 {
-    public OperationResult(Exception exception) => (Exception, Success,OnFailedMessage) = (exception, false,exception.Message);
-    public OperationResult(TResult result) => (Result,Success) = (result,false);
-    public OperationResult(string onFailedMessage) => (Success, OnFailedMessage) = (false, onFailedMessage);
+    public OperationResult() { }
+    public OperationResult(Exception exception) 
+        => (Exception, Success, OnFailedMessage) = (exception, false, exception.Message);
+    public OperationResult(string onFailedMessage) 
+        => (Success, OnFailedMessage) = (false, onFailedMessage);
+
+
 
     public bool Success { get; protected set; }
     public string OnFailedMessage { get; protected set; }
     public Exception Exception { get; protected set; }
+
+
+
+    public static OperationResult OnFailed(string message)
+        => new OperationResult(message);
+    public static OperationResult OnException(Exception exception)
+        => new OperationResult(exception);
+}
+public class OperationResult<TResult>:OperationResult
+{
+
+
+    public OperationResult(TResult result) : base()
+      => (Success, Result) = (true,result);
+    public OperationResult(string message) : base()
+  => (Success,OnFailedMessage) = (false,message);
+    public OperationResult(Exception ex) : base()
+=> (Success, Exception) = (false, ex);
+
+
     public TResult Result { get; protected set; }
 
-    public static OperationResult<TResult> OnSuccess(TResult result) => new OperationResult<TResult>(result);
-    public static OperationResult<TResult> OnFailed(string message) => new OperationResult<TResult>(message);
-    public static OperationResult<TResult> OnException(Exception exception) => new OperationResult<TResult>(exception);
+
+    public static  OperationResult<TResult> OnSuccess(TResult result) 
+        => new OperationResult<TResult>(result);
+    public static OperationResult<TResult> OnFailed(string message)
+    => new OperationResult<TResult>(message);
+    public static OperationResult<TResult> OnException(Exception exception)
+        => new OperationResult<TResult>(exception);
 }
