890d2150ca06243196c05bfd14ddfcff2b12fb7c
Moghaddm <mahdimoghaddam2005@gmail.com>
2023-07-05 08:28:53 +0330

    Add Operation Result

--- src/LMS.Application/Models/OperationResult.cs
+++ src/LMS.Application/Models/OperationResult.cs
@@ -1,11 +1,17 @@
 namespace BigBlueApi.Models;
-public class OperationResult<T>
+public class OperationResult<TResult>
 {
-    public OperationResult()
-    {
+    public OperationResult(Exception exception) => (Exception, Success,OnFailedMessage) = (exception, false,exception.Message);
 
-    }
-    public bool Success { get; set; }
-    public string Message { get; set; }
-    
+    public OperationResult(TResult result) => (Result,Success) = (result,false);
+    public OperationResult(string onFailedMessage) => (Success, OnFailedMessage) = (false, onFailedMessage);
+
+    public bool Success { get; protected set; }
+    public string OnFailedMessage { get; protected set; }
+    public Exception Exception { get; protected set; }
+    public TResult Result { get; protected set; }
+
+    public OperationResult<TResult> OnSuccess(TResult result) => new OperationResult<TResult>(result);
+    public OperationResult<TResult> OnFailed(string message) => new OperationResult<TResult>(message);
+    public OperationResult<TResult> OnException(Exception exception) => new OperationResult<TResult>(exception);
 }
