cf0dd5383a2b2c1713dfdc03e523529c7305c1a3
Moghaddm <mahdimoghaddam2005@gmail.com>
2023-07-05 08:29:09 +0330

    Update BBB Services

---
+++ src/LMS.Application/Services/Database/BBB/BBBMemberShipServiceImpl.cs
@@ -0,0 +1,40 @@
+﻿using BigBlueApi.Domain.IRepositories;
+using BigBlueApi.Domain.IRepository;
+using LIMS.Domain.Entity;
+using LIMS.Domain.IRepositories;
+
+namespace LIMS.Application.Services.Database.BBB
+{
+    public class MemberShipServiceImp
+    {
+        private readonly IUnitOfWork _uow;
+        private readonly IMemberShipRepository _memberShipRepository;
+        private readonly IUserRepository _userRepository;
+        private readonly IMeetingRepository _meetingRepository;
+
+        public MemberShipServiceImp(IUnitOfWork uow,
+            IMemberShipRepository repository,
+            IUserRepository userRepository,
+            IMeetingRepository meetingRepository)
+        {
+            _memberShipRepository = repository;
+            _uow = uow;
+            _userRepository = userRepository;
+            _meetingRepository = meetingRepository;
+        }
+
+        public async ValueTask<bool> CanJoinUserOnMeetingAsync(long id) =>
+            await _memberShipRepository.CanJoinUserOnMeetingAsync(id);
+
+        public async ValueTask<int> JoinUserAsync(string meetingId, int UserId)
+        {
+            var user = await _userRepository.GetUser(UserId);
+            var meeting = await _meetingRepository.FindByMeetingIdAsync(meetingId);
+            if (meeting is null)
+                return 0;
+            var Result = await _memberShipRepository.JoinUserAsync(meeting, user);
+            await _uow.SaveChangesAsync();
+            return Result;
+        }
+    }
+}
---
+++ src/LMS.Application/Services/Database/BBB/BBBUserServiceImpl.cs
@@ -0,0 +1,37 @@
+﻿using BigBlueApi.Application.DTOs;
+using BigBlueApi.Application.Mappers;
+using BigBlueApi.Domain.IRepository;
+using BigBlueApi.Persistence.Repository;
+using LIMS.Domain.Entity;
+
+namespace LIMS.Application.Services.Database.BBB
+{
+    public class BBBUserServiceImpl
+    {
+        private readonly IUnitOfWork _uow;
+        private readonly IUserRepository _Repository;
+
+        public BBBUserServiceImpl(IUserRepository repository, IUnitOfWork uow)
+        {
+            _uow = uow;
+            _Repository = repository;
+        }
+
+        public async ValueTask<int> CreateUser(UserAddEditDto user)
+        {
+            var newUser = UserDtoMapper.Map(user);
+            await _Repository.CreateUser(newUser);
+            await _uow.SaveChangesAsync();
+            return newUser.Id;
+        }
+
+        public async Task EditUser(int Id, UserAddEditDto user)
+        {
+            var newUser = UserDtoMapper.Map(user);
+            await _Repository.EditUser(Id, newUser);
+            await _uow.SaveChangesAsync();
+        }
+
+        public async ValueTask<User> Find(int userId) => await _Repository.Find(userId);
+    }
+}
