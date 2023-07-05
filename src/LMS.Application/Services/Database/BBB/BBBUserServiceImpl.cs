1f9ae8943f4ec818f7b068d6a72498924444f131
ArashJafary <aj2774382@gmail.com>
2023-07-05 13:52:00 +0330

    complate User Service

--- src/LMS.Application/Services/Database/BBB/BBBUserServiceImpl.cs
+++ src/LMS.Application/Services/Database/BBB/BBBUserServiceImpl.cs
@@ -1,37 +1,98 @@
 ﻿using BigBlueApi.Application.DTOs;
 using BigBlueApi.Application.Mappers;
 using BigBlueApi.Domain.IRepository;
-using BigBlueApi.Persistence.Repository;
+using BigBlueApi.Models;
 using LIMS.Domain.Entity;
 
 namespace LIMS.Application.Services.Database.BBB
 {
     public class BBBUserServiceImpl
     {
-        private readonly IUnitOfWork _uow;
+        private readonly IUnitOfWork _unitOfWork;
         private readonly IUserRepository _Repository;
 
         public BBBUserServiceImpl(IUserRepository repository, IUnitOfWork uow)
         {
-            _uow = uow;
+            _unitOfWork = uow;
             _Repository = repository;
         }
 
-        public async ValueTask<int> CreateUser(UserAddEditDto user)
+        public async ValueTask<OperationResult<long>> CreateUser(UserAddEditDto User)
         {
-            var newUser = UserDtoMapper.Map(user);
-            await _Repository.CreateUser(newUser);
-            await _uow.SaveChangesAsync();
-            return newUser.Id;
+            try
+            {
+                var NewUser = UserDtoMapper.Map(User);
+                await _Repository.CreateUser(NewUser);
+                await _unitOfWork.SaveChangesAsync();
+                if (NewUser.Id == 0)
+                    return OperationResult<long>.OnFailed("We have proplem to creat user in creat");
+                return OperationResult<long>.OnSuccess(NewUser.Id);
+            }
+            catch (Exception ex)
+            {
+                return OperationResult<long>.OnException(ex);
+            }
         }
 
-        public async Task EditUser(int Id, UserAddEditDto user)
+
+        public async ValueTask<OperationResult> EditUser(int Id, UserAddEditDto User)
         {
-            var newUser = UserDtoMapper.Map(user);
-            await _Repository.EditUser(Id, newUser);
-            await _uow.SaveChangesAsync();
+            try
+            {
+                await _Repository.EditUser(Id,UserDtoMapper.Map(User));
+                await _unitOfWork.SaveChangesAsync();
+                return new OperationResult(); 
+            }
+            catch (Exception ex)
+            {
+                return OperationResult.OnException(ex);
+            }
         }
 
-        public async ValueTask<User> Find(int userId) => await _Repository.Find(userId);
+        public async ValueTask<OperationResult<UserAddEditDto>> GetUser(int UserId)
+        {
+            try
+            {
+                var User = UserDtoMapper.Map(await _Repository.GetUser(UserId));
+                if (User is null)
+                    return OperationResult<UserAddEditDto>.OnFailed("user not find");
+                return OperationResult<UserAddEditDto>.OnSuccess(User);
+            }
+            catch (Exception ex)
+            {
+                return OperationResult<UserAddEditDto>.OnException(ex);
+            }
+        }
+
+        public async ValueTask<OperationResult<List<UserAddEditDto>>> GetUsers()
+        {
+            try
+            {
+                var Users= await _Repository.GetUsers();
+                var UserDto = new List<UserAddEditDto>();
+                foreach (var User in Users)
+                {
+                    UserDto.Add(UserDtoMapper.Map(User));
+                }
+                return OperationResult<List<UserAddEditDto>>.OnSuccess(UserDto);
+            }
+            catch(Exception ex)
+            { 
+                return OperationResult<List<UserAddEditDto>>.OnException(ex);
+            }
+        }
+
+        public async ValueTask<OperationResult<long>> DeleteUser(long Id)
+        {
+            try
+            {
+                var UserId = await _Repository.DeleteUser(Id);
+                return OperationResult<long>.OnSuccess(UserId);
+            }
+            catch (Exception ex)
+            {
+                return OperationResult<long>.OnException(ex);   
+            }
+        }
     }
 }
