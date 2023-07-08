b205a0994d20931b73c2be418b5e5fca803eed6d
Moghaddm <mahdimoghaddam2005@gmail.com>
2023-07-08 12:45:16 +0330

    Update Recording Entity Configuration

---
+++ src/LMS.Infrastructure/Persistence/Configuration/RecordEntityConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LIMS.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace LIMS.Infrastructure.Persistence.Configuration
+{
+    public class RecordEntityConfiguration : IEntityTypeConfiguration<Record>
+    {
+        public void Configure(EntityTypeBuilder<Record> builder)
+        {
+            builder.HasOne(record => record.Meeting).WithMany(playback => playback.Records).HasForeignKey("MeetingId");
+            builder.HasMany(record => record.Playbacks).WithOne(playback => playback.Record).HasForeignKey("RecordId");
+        }
+    }
+}
