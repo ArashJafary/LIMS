9be35a1dba3b528515af0631246663f0c4890ced
Mohammad Mahdi <MahdiMoghaddam2005@gmail.com>
2023-07-10 14:12:01 +0330

    Add Update Record Migrations

--- src/LIMS.Infrastructure/Migrations/20230710094342_Update_Records_ForPlaybacks.Designer.cs
+++ src/LIMS.Infrastructure/Migrations/20230710094342_Update_Records_ForPlaybacks.Designer.cs
@@ -4,6 +4,7 @@ using LIMS.Infrastructure.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace LIMS.Infrastructure.Migrations
 {
     [DbContext(typeof(LimsContext))]
-    partial class LimsContextModelSnapshot : ModelSnapshot
+    [Migration("20230710094342_Update_Records_ForPlaybacks")]
+    partial class Update_Records_ForPlaybacks
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
