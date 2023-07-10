a3c943d696e918e4502f140aed3276d9e3487aef
Moghaddm <mahdimoghaddam2005@gmail.com>
2023-07-10 10:35:04 +0330

    Can Join User Method is Updated

--- src/LIMS.Application/Services/Database/BBB/BBBMemberShipServiceImpl.cs
+++ src/LIMS.Application/Services/Database/BBB/BBBMemberShipServiceImpl.cs
@@ -20,12 +20,22 @@ namespace LIMS.Application.Services.Database.BBB
             IMeetingRepository meetings)
                 => (_uow, _memberShips, _users, _meetings) = (uow, memberShips, users, meetings);
 
-        public async ValueTask<OperationResult<bool>> CanJoinUserOnMeetingAsync(long id)
+        public async ValueTask<OperationResult<bool>> CanJoinUserOnMeetingAsync(long meetingId)
         {
             try
             {
-                var meeting = _meetings.FindAsync(id);
-                //var userCountOfMeeting = await meeting.Result.Users
+                var meeting = await _meetings.FindAsync(meetingId);
+                if (meeting is null)
+                    return OperationResult<bool>.OnFailed("Meeting Not Found.");
+
+                var memberShips = await _memberShips.GetMemberShipsAsync();
+                if (memberShips is null)
+                    return OperationResult<bool>.OnFailed("No Joining Found.");
+
+                var userCountOfMeeting = memberShips.Where(memberShip => memberShip.Meeting.Id == meetingId)!.Count();
+                if (meeting.LimitCapacity <= userCountOfMeeting)
+                    return OperationResult<bool>.OnFailed("Cannot Join On Meeting Because of Meeting Capacity is Fulled.");
+
                 return OperationResult<bool>.OnSuccess(true);
             }
             catch (Exception exception)
@@ -34,27 +44,27 @@ namespace LIMS.Application.Services.Database.BBB
             }
         }
 
-        //public async ValueTask<OperationResult<long>> JoinUserAsync(long userId, string meetingId)
-        //{
-        //    try
-        //    {
-        //        var user = await _users.GetUser(userId);
-        //        if (user is null)
-        //            return OperationResult<long>.OnFailed("User is Not Valid.");
-        //        var meeting = await _meetings.FindByMeetingIdAsync(meetingId);
-        //        if (meeting is null)
-        //            return OperationResult<long>.OnFailed("Meeting is Not Valid.");
+        public async ValueTask<OperationResult<long>> JoinUserAsync(long userId, string meetingId)
+        {
+            try
+            {
+                var user = await _users.GetUser(userId);
+                if (user is null)
+                    return OperationResult<long>.OnFailed("User is Not Valid.");
+                var meeting = await _meetings.FindByMeetingIdAsync(meetingId);
+                if (meeting is null)
+                    return OperationResult<long>.OnFailed("Meeting is Not Valid.");
 
-        //        var result = await _memberShips.JoinUserAsync(meeting, user);
-        //        await _uow.SaveChangesAsync();
+                var result = await _memberShips.CreateMemeberShipForMeetingAsync(meeting, user);
+                await _uow.SaveChangesAsync();
 
-        //        return OperationResult<long>.OnSuccess(result);
-        //    }
-        //    catch (Exception exception)
-        //    {
-        //        return OperationResult<long>.OnException(exception);
-        //    }
-        //}
+                return OperationResult<long>.OnSuccess(result);
+            }
+            catch (Exception exception)
+            {
+                return OperationResult<long>.OnException(exception);
+            }
+        }
 
         public async ValueTask<OperationResult> BanUserFromMeeting(long userId, string meetingId)
         {
@@ -67,7 +77,10 @@ namespace LIMS.Application.Services.Database.BBB
                 if (meeting is null)
                     return OperationResult.OnFailed("Meeting is Not Valid.");
 
-                var member =  await _memberShips.GetMemberShip(user.Id, meeting.Id);
+                var member =  await _memberShips.GetMemberShipAsync(user.Id, meeting.Id);
+                if (member is null)
+                    return OperationResult.OnFailed("Your Considered Joining Was Not Found.");
+
                 await member.BanUser();
                 await _uow.SaveChangesAsync();
 
