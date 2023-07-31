b94439a05cdf88a7fab1346e93da2a97907af9ae
Mohammad Mahdi <MahdiMoghaddam2005@gmail.com>
2023-07-31 08:48:32 +0330

    Update Migrations For Roch Modeling

--- src/LIMS.Infrastructure/Persistence/Migrations/20230731051809_UpdateRichModelMeeting.Designer.cs
+++ src/LIMS.Infrastructure/Persistence/Migrations/20230731051809_UpdateRichModelMeeting.Designer.cs
@@ -4,6 +4,7 @@ using LIMS.Infrastructure.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace LIMS.Infrastructure.Persistence.Migrations
 {
     [DbContext(typeof(LimsContext))]
-    partial class LimsContextModelSnapshot : ModelSnapshot
+    [Migration("20230731051809_UpdateRichModelMeeting")]
+    partial class UpdateRichModelMeeting
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
